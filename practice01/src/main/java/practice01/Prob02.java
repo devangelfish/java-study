7fa3d8f83ad851a14bba94c55dec193c9d0f782d
Jaebeom <Jaebeom@DESKTOP-70F1IKP>
2020-12-10 21:14:28 +0900

    practice01 - 3의 배수

--- practice01/src/main/java/practice01/Prob02.java
+++ practice01/src/main/java/practice01/Prob02.java
@@ -1,8 +1,16 @@
 package practice01;
 
+import java.util.Scanner;
+
 public class Prob02 {
-
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("수를 입력하시오 : ");
+		int num = scanner.nextInt();
+		
+		if(num % 3 == 0 && num > 0) {
+			System.out.println("3의 배수입니다");
+		} 
 	}
-
 }
