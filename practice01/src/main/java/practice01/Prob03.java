72faac246192a8c25e84e93b46b64def5e7cdd45
Jaebeom <Jaebeom@DESKTOP-70F1IKP>
2020-12-10 21:56:42 +0900

    practice01 - 369게임

--- practice01/src/main/java/practice01/Prob03.java
+++ practice01/src/main/java/practice01/Prob03.java
@@ -2,5 +2,17 @@ package practice01;
 
 public class Prob03 {
 	public static void main(String[] args) {
+		for(int i = 0; i < 100; i++) {
+			String sNum = String.valueOf(i);
+			String even = "";
+			for(int j = 0; j < sNum.length(); j++) {			
+				if(sNum.charAt(j) == '3' || sNum.charAt(j) == '6' || sNum.charAt(j) == '9') {
+					even += "짝";
+				} else
+					continue;
+			}
+			if(!even.isEmpty())
+				System.out.println(sNum + " " + even);
+		}
 	}
 }
