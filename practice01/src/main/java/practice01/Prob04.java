e17cf8625430554b56122c32a87c09ab924faa24
Jaebeom <Jaebeom@DESKTOP-70F1IKP>
2020-12-10 23:19:53 +0900

    practice01 - 숨겨진 카드 맞추기 게임

--- practice01/src/main/java/practice01/Prob04.java
+++ practice01/src/main/java/practice01/Prob04.java
@@ -1,6 +1,36 @@
 package practice01;
 
+import java.util.Scanner;
+
 public class Prob04 {
 	public static void main(String[] args) {
+		int i = 1;
+		int randNum = 0;
+		int rightAnswer;
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			if (randNum == 0) {
+				randNum = (int) (Math.random() * 100) + 1;
+				System.out.println("수를 결정하였습니다. 맞추어보세요\n1-100");
+			}
+			
+			System.out.print(i + ">>");
+			rightAnswer = scanner.nextInt();
+			
+			if(randNum > rightAnswer) {
+				System.out.println("더 높게");
+			} else if(randNum < rightAnswer) {
+				System.out.println("더 낮게");
+			} else {
+				scanner.nextLine();
+				System.out.println("맞았습니다.");
+				System.out.print("다시하시겠습니까?(y/n)>>");
+				String again = scanner.nextLine();
+					if(again.equals("y")) {
+						randNum = 0;
+					} else break;
+			}
+		}
 	}
 }
