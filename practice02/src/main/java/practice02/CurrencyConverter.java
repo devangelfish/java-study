8a799552e152db012b265c8eaca34d11b0ea1a66
Jaebeom <Jaebeom@DESKTOP-70F1IKP>
2020-12-11 21:07:35 +0900

    practice02 - CurrenyConverter 문제

--- practice02/src/main/java/practice02/CurrencyConverter.java
+++ practice02/src/main/java/practice02/CurrencyConverter.java
@@ -1,5 +1,19 @@
 package practice02;
 
 public class CurrencyConverter {
+	private static double rate;
 
+	public static double toDollar(double won) {
+		return won/rate;
+		// 한국 원화를 달러로 변환
+	}
+
+	public static double toKWR(double dollar) {
+		return dollar*rate;
+		// 달러를 한국 원화로 변환
+	}
+
+	public static void setRate(double r) {
+		rate = r;
+	}
 }
--- practice02/src/main/java/practice02/Prob04.java
+++ practice02/src/main/java/practice02/Prob04.java
@@ -2,15 +2,15 @@ package practice02;
 
 public class Prob04 {
 	public static void main(String[] args) {
-		CurrencyConverter.set(1084.52);
+		CurrencyConverter.setRate(1084.52);
 		
-		double w1 = 100000;
-		double d1 = CurrencyConverter.toDollar(w);
+		double w1 = 1000000;
+		double d1 = CurrencyConverter.toDollar(w1);
 		
-		System.out.println("");
+		System.out.println("백만원은 " + d1 + "달러입니다.");
 		
 		double d2 = 100;
-		double w2 = CurrencyConverter.toKWR();
-		System.out.println("");
+		double w2 = CurrencyConverter.toKWR(d2);
+		System.out.println("백달러는 " + w2 + "원입니다.");
 	}
 }
