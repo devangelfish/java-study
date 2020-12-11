c520346798731c7be5507ea8343234dca2c16ac6
Jaebeom <Jaebeom@DESKTOP-70F1IKP>
2020-12-11 20:45:39 +0900

    practice02 - Goods 객체 문제

--- practice02/src/main/java/practice02/Goods.java
+++ practice02/src/main/java/practice02/Goods.java
@@ -1,5 +1,23 @@
 package practice02;
 
 public class Goods {
-
+	private String name;
+	private int price;
+	private int stockCount;
+		
+	public Goods(String name, int price, int stockCount) {
+		this.name = name;
+		this.price = price;
+		this.stockCount = stockCount;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public int getStockCount() {
+		return stockCount;
+	}	
 }
--- practice02/src/main/java/practice02/Prob02.java
+++ practice02/src/main/java/practice02/Prob02.java
@@ -2,9 +2,9 @@ package practice02;
 
 import java.util.Scanner;
 
-public class Prob01 {
+public class Prob02 {
 	public static void main(String[] args) {
-		final int GOODS_COUNT = 1;
+		final int GOODS_COUNT = 3;
 		
 		Goods[] goods = new Goods[GOODS_COUNT];
 		
@@ -15,11 +15,12 @@ public class Prob01 {
 			String[] infos = info.split(" ");
 			String name = infos[0];
 			int price = Integer.parseInt(infos[1]);
-			int stockCount = Integer.parseInt(infos[1]);
+			int stockCount = Integer.parseInt(infos[2]);
+			goods[i] = new Goods(name, price, stockCount);
 		}
 		
 		for(int i = 0; i < GOODS_COUNT; i++) {
-			
+			System.out.println(goods[i].getName() + "(가격:" + goods[i].getPrice() + "원)이 " + goods[i].getStockCount() + "개 입고 되었습니다.");
 		}
 		
 		scanner.close();
