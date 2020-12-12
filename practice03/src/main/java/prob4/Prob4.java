2cc17e72a9f1866899f1fe7d2d0054e9c3cd199a
Jaebeom <Jaebeom@DESKTOP-70F1IKP>
2020-12-12 14:50:39 +0900

    practice03 - 5개의 숫자 평균 구하는 문제

--- practice03/src/main/java/prob4/Prob4.java
+++ practice03/src/main/java/prob4/Prob4.java
@@ -8,12 +8,22 @@ public class Prob4 {
         Scanner in = new Scanner( System.in );
         int intArray[] = new int[5];
         double sum = 0;
-
+        
+        System.out.println("5개의 숫자를 입력하세요.");
+        
         /* 키보드에서 배열 크기만큼 입력 받아 배열에 정장하는 코드 */
+        for(int i = 0; i < intArray.length; i++) {
+        	intArray[i] = in.nextInt();
+        	in.nextLine();
+        }
 
         /* 배열에 저장된 정수 값 더하기 */
+        for(int num : intArray) {
+        	sum += num;
+        }
 
         /* 출력 */
+        System.out.println("평균은 " + sum / intArray.length + " 입니다.");
 	}
 
 }
