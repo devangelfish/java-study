7fe8c778c99ec0809498bea91966ebcdca8ff5a6
Jaebeom <Jaebeom@DESKTOP-70F1IKP>
2020-12-12 15:29:41 +0900

    practice04 - 홀수 합, 짝수 합 문제

--- practice04/src/main/java/prob1/Prob1.java
+++ practice04/src/main/java/prob1/Prob1.java
@@ -6,11 +6,17 @@ public class Prob1 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
-		System.out.print( "숫자를 입력하세요 :" );
+		System.out.print( "숫자를 입력하세요 : " );
 		
 		int inputNumber = scanner.nextInt();
-
-		/* 코드를 추가합니다 */
+		int sum = 0;
+		for(int i = 0; i <= inputNumber; i++) {
+			if(inputNumber % 2 == 0 && i % 2 == 0) {
+				sum += i;			
+			} else if(inputNumber % 2 == 1 && i % 2 == 1) {
+				sum += i;
+			}
+		}
+		System.out.println("결과 값 : " + sum);
 	}
-
 }
