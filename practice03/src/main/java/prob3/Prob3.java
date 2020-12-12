0d358dd1a25c86c3decb6691d329a7af1be7e47e
Jaebeom <Jaebeom@DESKTOP-70F1IKP>
2020-12-12 14:40:11 +0900

    practice03 - 문자열 뒤집기 문제

--- practice03/src/main/java/prob3/Prob3.java
+++ practice03/src/main/java/prob3/Prob3.java
@@ -13,11 +13,17 @@ public class Prob3 {
 	}
 	
 	public static char[] reverse(String str){
-		
-		return null;
+		char[] array = new char[str.length()]; 
+		for(int i = 0; i < str.length(); i++) {
+			array[(str.length() - 1) - i] = str.charAt(i);
+		}
+		return array;
 	}
 	
 	public static void printCharArray(char[] array){
-		
+		for(int i = 0; i < array.length; i++) {
+			System.out.print(array[i]);
+		}
+		System.out.println();
 	}
 }
