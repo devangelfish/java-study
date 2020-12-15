7e26c71ce1714f0975ddc918df89103e3458a8b7
Jaebeom <Jaebeom@DESKTOP-70F1IKP>
2020-12-15 16:47:11 +0900

    400, 404 메시지 처리

--- network/src/main/java/http/RequestHandler.java
+++ network/src/main/java/http/RequestHandler.java
@@ -1,65 +1,132 @@
 package http;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
+	private static final String DOCUMENT_ROOT = "./webapp";
 	private Socket socket;
-	
-	public RequestHandler( Socket socket ) {
+
+	public RequestHandler(Socket socket) {
 		this.socket = socket;
 	}
-	
+
 	@Override
 	public void run() {
 		try {
 			// get IOStream
-			OutputStream outputStream = socket.getOutputStream(); // 웹서버는 바이트
-			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));// 버퍼리더는 개행이 있어야 보냄
+			OutputStream outputStream = socket.getOutputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
 
 			// logging Remote Host IP Address & Port
-			InetSocketAddress inetSocketAddress = ( InetSocketAddress )socket.getRemoteSocketAddress();
-			consoleLog( "connected from " + inetSocketAddress.getAddress().getHostAddress() + ":" + inetSocketAddress.getPort() );
-			
+			InetSocketAddress inetSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
+			consoleLog("connected from " + inetSocketAddress.getAddress().getHostAddress() + ":"
+					+ inetSocketAddress.getPort());
+
+			String request = null;
 			while (true) {
-				// 2. 데이터 읽기
 				String line = br.readLine();
+
+				// 브라우저가 연결을 끊음
 				if (line == null) {
-					// Client가 소켓을 정상 종료
-					System.out.println("[server] closed by client");
+					consoleLog("closed by client");
 					break;
 				}
 
-				consoleLog("received:" + line);
+				// Request Header의 첫번째 라인만 읽음
+				if (request == null) {
+					request = line;
+					break;
+				}
 			}
-			
-			// 예제 응답입니다.
-			// 서버 시작과 테스트를 마친 후, 주석 처리 합니다.
-//			outputStream.write( "HTTP/1.1 200 OK\r\n".getBytes( "UTF-8" ) );
-//			outputStream.write( "Content-Type:text/html; charset=utf-8\r\n".getBytes( "UTF-8" ) );
-//			outputStream.write( "\r\n".getBytes() );
-//			outputStream.write( "<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된 것입니다.</h1>".getBytes( "UTF-8" ) );
 
-		} catch( Exception ex ) {
-			consoleLog( "error:" + ex );
+			consoleLog(request);
+
+			String[] tokens = request.split(" ");
+			if ("GET".equals(tokens[0])) {
+				responseStaticResource(outputStream, tokens[1], tokens[2]);
+			} else { // POST, DELETE, PUT, HEAD, CONNECT
+				
+				// HTTP/1.1 400 Bad Request\r\n
+				// Content-Type:text/html; charset=utf-8\r\n
+				// \r\n
+				// html 에러 문서(./webapp/error/400.html)
+				response400Error(outputStream, tokens[2]);
+			}
+
+		} catch (Exception ex) {
+			consoleLog("error:" + ex);
 		} finally {
 			// clean-up
-			try{
-				if( socket != null && socket.isClosed() == false ) {
+			try {
+				if (socket != null && socket.isClosed() == false) {
 					socket.close();
 				}
-				
-			} catch( IOException ex ) {
-				consoleLog( "error:" + ex );
+			} catch (IOException ex) {
+				consoleLog("error:" + ex);
 			}
-		}			
+		}
 	}
 
-	public void consoleLog( String message ) {
-		System.out.println( "[RequestHandler#" + getId() + "] " + message );
+	public void responseStaticResource(OutputStream outputStream, String uri, String protocol) throws IOException {
+		if ("/".equals(uri)) {
+			uri = "/index.html";
+		}
+
+		File file = new File(DOCUMENT_ROOT + uri);
+		if (!file.exists()) {		
+			// HTTP/1.1 404 Not Found\r\n
+			// Content-Type:text/html; charset=utf-8\r\n
+			// \r\n
+			// html 에러 문서(./webapp/error/404.html)
+
+			// response404Error();
+			response404Error(outputStream, protocol);
+			return;
+		}
+
+		// nio
+		byte[] body = Files.readAllBytes(file.toPath());
+		String contentType = Files.probeContentType(file.toPath());
+
+		// response
+		outputStream.write((protocol + " 200 OK\r\n").getBytes("UTF-8"));
+		outputStream.write(("Content-Type:" + contentType + "; charset=utf-8\r\n").getBytes("UTF-8"));
+		outputStream.write("\r\n".getBytes("UTF-8"));
+		outputStream.write(body);
+	}
+
+	public void response400Error(OutputStream outputStream, String protocol) throws IOException {
+		String uri = "/error/400.html";
+		File file = new File(DOCUMENT_ROOT + uri);
+		byte[] body = Files.readAllBytes(file.toPath());
+		String contenType = Files.probeContentType(file.toPath());
+
+		outputStream.write((protocol + " 400 Bad Request\r\n").getBytes("UTF-8"));
+		outputStream.write(("Content-Type:" + contenType + "; charset=utf-8\r\n").getBytes("UTF-8"));
+		outputStream.write("\r\n".getBytes("UTF-8"));
+		outputStream.write(body);
+	}
+
+	public void response404Error(OutputStream outputStream, String protocol) throws IOException {
+		String uri = "/error/404.html";
+		File file = new File(DOCUMENT_ROOT + uri);
+		byte[] body = Files.readAllBytes(file.toPath());
+		String contenType = Files.probeContentType(file.toPath());
+
+		outputStream.write((protocol + " 404 Not Found\r\n").getBytes("UTF-8"));
+		outputStream.write(("Content-Type:" + contenType + "; charset=utf-8\r\n").getBytes("UTF-8"));
+		outputStream.write("\r\n".getBytes("UTF-8"));
+		outputStream.write(body);
+	}
+
+	public void consoleLog(String message) {
+		System.out.println("[RequestHandler#" + getId() + "] " + message);
 	}
 }
